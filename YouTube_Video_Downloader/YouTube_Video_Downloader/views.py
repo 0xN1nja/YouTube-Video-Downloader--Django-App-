c97b1c83ed1fbb5592a7f2e27e9cf030da1038d0
Abhimanyu <speedcuberabhi@gmail.com>
2021-07-02 17:26:20 +0530

    Final Version (Needs More Changes Tho)

--- YouTube_Video_Downloader/YouTube_Video_Downloader/views.py
+++ YouTube_Video_Downloader/YouTube_Video_Downloader/views.py
@@ -1,14 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from pytube import YouTube
+from pathlib import Path
+import os
 def index(request):
     return render(request,"index.html")
 def analyze_to_mp3(request):
     link=request.POST.get("link")
     try:
         yt=YouTube(link)
+        ys=yt.streams.get_by_itag("140")
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        ys.download(BASE_DIR)
+        params={"status":"Successfully Downloaded !","location":f"{os.path.join(BASE_DIR,yt.title)}.mp3","Format":"MP3","Title":yt.title,"Description":yt.description,"Rating":yt.rating,"Duration":yt.length}
     except:
         return HttpResponse("<h1>403<br>Please Enter A Valid URL</h1>")
-    return render(request,"analyze.html")
+    return render(request,"analyze_to_mp3.html",params)
 def analyze_to_mp4(request):
-    pass
+    link=request.POST.get("link")
+    print(link)
+    try:
+        yt=YouTube(link)
+        ys=yt.streams.get_by_itag("22")
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        ys.download(BASE_DIR)
+        params={"status":"Successfully Downloaded !","location":f"{os.path.join(BASE_DIR,yt.title)}.mp4","Format":"MP4","Title":yt.title,"Description":yt.description,"Rating":yt.rating,"Duration":yt.length}
+    except:
+        return HttpResponse("<h1>403<br>Please Enter A Valid URL</h1>")
+    return render(request,"analyze_to_mp4.html",params)
